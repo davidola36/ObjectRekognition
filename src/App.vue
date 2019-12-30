7a99b036cd57173c4ac7f15ac0f41aed21555712
David Adeoye <davidola360@gmail.com>
2019-12-30 09:46:41 +0100

    initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,131 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="rek--top">
+      <input type="file" id="filePicker" ref="img" @change="imageHandler" style="visibility:hidden">
+      <h1 class="rek--title">Image Detection with Rekognition</h1>
+
+
+      <div class="rek--contentCont">
+        <div class="rek--contentCont1">
+          <div class="search--box" @click="openFilePicker">
+            <img :src="captured" alt="Search image" v-if="captured && (loadState=='initial' || loadState == 'complete')" class="search--img"/>
+            <span v-if="!captured && loadState=='initial'">click here to add image</span>
+            <span v-if="loadState=='loading'">Searching for image</span>
+          </div>
+          <div class="search--itemsCont">
+            <button class="button--primary" :disabled="!captured" @click="searchItem">Find Item</button>
+            <button class="button--secondary" @click="reset">Reset Image</button>
+          </div>
+        </div>
+        <div class="rek--contentCont1">
+          <div v-if="loadState=='initial' || loadState=='loading'">
+            <h4>No Search made</h4>
+          </div>
+          <div v-if="loadState=='complete'">
+            <div v-if="!labels">No item was rekognized in the image provided</div>
+            <div v-else>
+              <h4 class="search--title">These items were found in the image</h4>
+              <div class="search--item" style="font-weight:bold"><span>Name</span> <span>Confidence Level</span></div>
+              <div v-for="(item, i) in labels" :key="i" class="search--item"><span>{{item.Name}}</span> <span>{{item.Confidence}}</span></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      captured: null,
+      loadState: 'initial',
+      file: null,
+      labels: []
+    }
+  },
+  methods: {
+    searchItem() {
+      this.loadState = 'loading'
+      this.uploadImage()
+    },
+    uploadImage() {
+       window.s3.upload(
+        {
+          Key: this.file.name,
+          Body: this.file,
+          ACL: "public-read"
+        },
+        (err, data) => {
+          if (err) {
+            return console.log("Ran into an error: ", err);
+          }
+          console.log(data, "upload complete")
+          this.detectLabels(data)
+        })
+    },
+    detectLabels(data) {
+      console.log('detecting labels')
+      var params = {
+      Image: {
+        S3Object: {
+          Bucket: "peexoo.useridcards", 
+          Name: data.Key
+        }
+        }, 
+        MaxLabels: 123, 
+        MinConfidence: 70
+      };
+      let vm = this
+      window.rekognition.detectLabels(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else    {
+          vm.loadState = 'complete'
+          vm.labels = data.Labels
+          console.log(data, "done")
+        }        // successful response
+        /*
+        data = {
+          Labels: [
+            {
+            Confidence: 99.25072479248047, 
+            Name: "People"
+          }, 
+            {
+            Confidence: 99.25074005126953, 
+            Name: "Person"
+          }
+          ]
+        }
+        */
+      });
+    },
+    reset() {
+      this.captured = null
+      this.loadState = "initial"
+      this.labels = []
+    },
+    imageHandler(e) {
+      var files = e.target.files;
+      this.file = files[0]
+      this.previewImage(files[0])
+    },
+    previewImage(image) {
+      const reader = new FileReader();
+      reader.readAsDataURL(image);
+      let vm = this
+      reader.onloadend = () => {
+          let result = reader.result
+          vm.captured = result;
+          // this.image = image;
+      }
+    },
+    openFilePicker() {
+      document.getElementById("filePicker").click()
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+<style lang="css" scoped>
+  @import url("style.css");
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'aws-sdk/lib/node_loader'; // Hack needed before the first import
+import { config } from 'aws-sdk/lib/core'; // or any other `aws-sdk` export
+// import S3 from 'aws-sdk/clients/s3';
+
+
+import Rekognition from 'aws-sdk/clients/rekognition'
+
+import AWS from "aws-sdk";
+
+
+var albumBucketName = "ADDBUCKETNAME";
+var bucketRegion = "eu-west-1";
+var IdentityPoolId = "ADDIDENTITYPOOLID";
+// const signedUrlExpireSeconds = 60 * 5
+config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  }),
+
+});
+var rekognition = new Rekognition({
+  region: 'eu-west-1',
+  apiVersion: '2016-06-27'
+});
+
+let s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  params: { Bucket: albumBucketName }
+});
+// console.log(getDropzone('brad.jpg'));
+// console.log(res);
+window.s3 = s3;
+window.rekognition = rekognition
 
 Vue.config.productionTip = false
 
